0188bc00aea4724728b6e62d3cfa20c1c6359789
lichen <lichenarthurml@gmail.com>
2017-06-24 16:33:56 +0800

    修改网络结构

--- examples/mnist/models/model_cnn.py
+++ examples/mnist/models/model_cnn.py
@@ -11,18 +11,17 @@ class ModelCNNBase(torch.nn.Module):
 	def __init__(self, opt):
 		super(ModelCNNBase, self).__init__()
 		self.opt = opt
-		self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
-		self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
-		self.conv2_drop = nn.Dropout2d()
-		self.fc1 = nn.Linear(320, 50)
-		self.fc2 = nn.Linear(50, 10)
+		self.conv1 = nn.Conv2d(1, 16, kernel_size=5, padding=2)
+		self.conv2 = nn.Conv2d(16, 32, kernel_size=5, padding=2)
+		self.fc1 = nn.Linear(32*7*7, 64)
+		self.fc2 = nn.Linear(64, 10)
 
 	def forward(self, x, training=True):
+		x = x / 255.0 - 0.5
 		x = F.relu(F.max_pool2d(self.conv1(x), 2))
-		x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
-		x = x.view(-1, 320)
+		x = F.relu(F.max_pool2d(self.conv2(x), 2))
+		x = x.view(-1, 32*7*7)
 		x = F.relu(self.fc1(x))
-		x = F.dropout(x, training=self.training)
 		x = self.fc2(x)
 		return F.log_softmax(x)
 
@@ -42,12 +41,16 @@ class ModelCNN(lake.torch.network.Base):
 		if self.use_cuda:
 			x, y = x.cuda(), y.cuda()
 		x, y = Variable(x, volatile=not is_train), Variable(y)
-		output = self.forward(x)
+		output = self.model.forward(x, is_train)
 		error = self.critera(output, y)
+
+		pred = output.data.max(1)[1]
+		correct = float(pred.eq(y.data).cpu().sum()) / y.data.size()[0]
+
 		if is_train:
-			return dict(loss=error)
+			return dict(loss=error, correct=correct)
 		else:
-			return dict(loss=float(error.data[0]))
+			return dict(loss=float(error.data[0]), correct=correct)
 
 	def train(self, data):
 		return self.run(data)
