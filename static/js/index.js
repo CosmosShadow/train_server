c392144100d87a8f586da42e8dc91a3176eb52a3
lichen <lichenarthurml@gmail.com>
2017-07-11 22:33:23 +0800

    cookie

--- static/js/index.js
+++ static/js/index.js
@@ -1,25 +1,34 @@
 $(document).ready(function() {
-	var data = ['A', 'B', 'C', 'D']
-	var index = 1
+	var projects = []
+	var index = Cookies.get('index')
+	if (index == null) {
+		index = 0
+	}
 
-	function reload() {
+	$.getJSON("/data/projects.json", function(result) {
+		console.log(result)
+		projects = result
+		fresh_nav()
+	})
+
+	function fresh_nav() {
 		// 清空元素后新生成
 		$("#sidebar").html('')
-		for (var i = 0; i < data.length; i++) {
+		for (var i = 0; i < projects.length; i++) {
 			the_class = 'nav_no_active'
 			if (i == index) {
 				the_class = 'nav_active'
 			}
-			$("#sidebar").append($("<li></li>").append($("<a role='button' class='nav_li'></a>").addClass(the_class).text(data[i]).attr('index', i)))
+			project_name = projects[i]['name']
+			$("#sidebar").append($("<li></li>").append($("<a role='button' class='nav_li'></a>").addClass(the_class).text(project_name).attr('index', i)))
 		}
 
 		// 绑定按钮
 		$(".nav_li").click(function(event) {
 			index = parseInt($(event.target).attr('index'))
+			Cookies.set('index', index, { expires: 7 })
 			console.log(index)
-			reload()
+			fresh_nav()
 		});
 	}
-
-	reload()
 })
